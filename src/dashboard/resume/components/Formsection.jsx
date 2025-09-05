f62e8f4716b01ef09b6ae9cd204eb07490ea521a
saptarshidey3000 <saptarshidey3000@gmail.com>
2025-09-06 01:24:33 +0530

    form section 1st part

--- src/dashboard/resume/components/Formsection.jsx
+++ src/dashboard/resume/components/Formsection.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Personaldetails from './forms/Personaldetails'
 import { Button } from '@/components/ui/button'
-import { ArrowRight } from 'lucide-react'
+import { ArrowLeft, ArrowRight } from 'lucide-react'
 import { LayoutGrid } from 'lucide-react'
 const Formsection = () => {
+  const [activeformindex , setactiveformindex] = useState(1);
+  const [enablenext , setenablenext] = useState(false);
   return (
     <div>
       <div className='flex justify-between items-center mb-5'>
         <Button variant="outline" size="sm"
          className="flex gap-2"> <LayoutGrid/> Theme</Button>
-        <div>
-          <Button className="flex gap-2" size="sm" >Next 
+        <div className='flex gap-2'>
+          {activeformindex>1
+           && <Button size="sm"
+           onClick={()=>setactiveformindex(activeformindex-1)}><ArrowLeft/></Button>}
+          <Button 
+          disabled={!enablenext}
+          className="flex gap-2" size="sm"
+           onClick={()=>setactiveformindex(activeformindex+1)}>Next 
             <ArrowRight/> </Button>
         </div>
       </div>
       {/* Personal details  */}
-      <Personaldetails/>
+        {activeformindex==1?  <Personaldetails enablenext={(v)=>setenablenext(v)} /> :null}
       {/* summary  */}
 
       {/* experience */}
--- src/dashboard/resume/components/forms/Personaldetails.jsx
+++ src/dashboard/resume/components/forms/Personaldetails.jsx
@@ -1,12 +1,61 @@
 import { ResumeInfoContext } from '@/context/ResumeInfoContext'
 import React, { useContext } from 'react'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
-const Personaldetails = () => {
-    const {resumeinfo , setresumeinfo} = useContext(ResumeInfoContext)
+const Personaldetails = ({enablenext}) => {
+  const { resumeinfo, setresumeinfo, activeIndex, setActiveIndex } =
+    useContext(ResumeInfoContext)
+
+  const handleinputchange = (e) => {
+    enablenext(false)
+    const { name, value } = e.target
+    setresumeinfo({
+      ...resumeinfo,
+      [name]: value,
+    })
+  }
+
+  const onSave = (e) => {
+    e.preventDefault()
+    enablenext(true);
+  }
+
   return (
-    <div className='p-5 shadow-lg rounded-lg border-t-primary border-t-4 m-t-10'>
-        <h2 className='font-bold text-lg'>Personal Details</h2>
-        <p>Get started with basic information</p>
+    <div className="p-5 shadow-lg rounded-lg border-t-primary border-t-4 m-t-10">
+      <h2 className="font-bold text-lg">Personal Details</h2>
+      <p>Get started with basic information</p>
+      <form onSubmit={onSave}>
+        <div className="grid grid-cols-2 mt-5 gap-3">
+          <div>
+            <label className="text-sm">First Name</label>
+            <Input name="firstName" required onChange={handleinputchange} />
+          </div>
+          <div>
+            <label className="text-sm">Last Name</label>
+            <Input name="lastName" required onChange={handleinputchange} />
+          </div>
+          <div className="col-span-2">
+            <label className="text-sm">Job Title</label>
+            <Input name="jobTitle" required onChange={handleinputchange} />
+          </div>
+          <div className="col-span-2">
+            <label className="text-sm">Address</label>
+            <Input name="address" required onChange={handleinputchange} />
+          </div>
+          <div>
+            <label className="text-sm">Phone</label>
+            <Input name="phone" required onChange={handleinputchange} />
+          </div>
+          <div>
+            <label className="text-sm">Email</label>
+            <Input name="email" required onChange={handleinputchange} />
+          </div>
+        </div>
+        <div className="mt-3 flex justify-end">
+          <Button type="submit">Save</Button>
+        </div>
+      </form>
     </div>
   )
 }
